a4cbe27c47eec50abb3ea5e1f196a8c35d3f4f50
LeandroCesarRamos <leandrocesarramos@gmail.com>
2025-10-09 10:26:13 -0300

    Add Files

--- Python/TicTacToe/main.py
+++ Python/TicTacToe/main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import random
 
 # Cores do portfólio
 BG_COLOR = (24, 26, 32)     # #181a20
@@ -56,11 +57,16 @@ def draw_board():
         status = small_font.render(f'Player turn: {current_player}', True, X_COLOR if current_player == 'X' else O_COLOR)
     screen.blit(status, (WIDTH//2 - status.get_width()//2, status_y))
     # Botão reiniciar
-    btn_rect = pygame.Rect(WIDTH//2-80, HEIGHT-50, 160, 36)
+    btn_rect = pygame.Rect(WIDTH//2-180, HEIGHT-50, 160, 36)
     pygame.draw.rect(screen, X_COLOR, btn_rect, border_radius=8)
     btn_text = pygame.font.SysFont('Roboto', 24, bold=True).render('Restart', True, CARD_COLOR)
     screen.blit(btn_text, (btn_rect.x + btn_rect.width//2 - btn_text.get_width()//2, btn_rect.y + 6))
-    return btn_rect
+    # Botão voltar
+    back_rect = pygame.Rect(WIDTH//2+30, HEIGHT-50, 160, 36)
+    pygame.draw.rect(screen, X_COLOR, back_rect, border_radius=8)
+    back_text = pygame.font.SysFont('Roboto', 24, bold=True).render('Return', True, CARD_COLOR)
+    screen.blit(back_text, (back_rect.x + back_rect.width//2 - back_text.get_width()//2, back_rect.y + 6))
+    return btn_rect, back_rect
 
 def check_winner():
     wins = [
@@ -82,6 +88,54 @@ def reset_board():
     game_over = False
     winner = None
 
+def minimax(board, player):
+    winner = check_winner_minimax(board)
+    if winner == 'O':
+        return 1
+    elif winner == 'X':
+        return -1
+    elif winner == 'Draw':
+        return 0
+    moves = []
+    for i in range(9):
+        if board[i] == '':
+            board_copy = board[:]
+            board_copy[i] = player
+            score = minimax(board_copy, 'X' if player == 'O' else 'O')
+            moves.append((score, i))
+    if player == 'O':
+        max_move = max(moves)
+        return max_move[0] if len(moves) > 1 else max_move[0]
+    else:
+        min_move = min(moves)
+        return min_move[0] if len(moves) > 1 else min_move[0]
+
+def best_move(board):
+    moves = []
+    for i in range(9):
+        if board[i] == '':
+            board_copy = board[:]
+            board_copy[i] = 'O'
+            score = minimax(board_copy, 'X')
+            moves.append((score, i))
+    if moves:
+        best = max(moves)
+        return best[1]
+    return None
+
+def check_winner_minimax(board):
+    wins = [
+        [0,1,2],[3,4,5],[6,7,8],
+        [0,3,6],[1,4,7],[2,5,8],
+        [0,4,8],[2,4,6]
+    ]
+    for a,b,c in wins:
+        if board[a] == board[b] == board[c] != '':
+            return board[a]
+    if '' not in board:
+        return 'Draw'
+    return None
+
 # Loop principal
 while True:
     for event in pygame.event.get():
@@ -89,7 +143,7 @@ while True:
             pygame.quit()
             sys.exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if not game_over:
+            if not game_over and current_player == 'X':
                 mx, my = event.pos
                 board_width = 3 * CELL_SIZE + 2 * MARGIN
                 start_x = (WIDTH - board_width) // 2
@@ -104,11 +158,25 @@ while True:
                         if winner:
                             game_over = True
                         else:
-                            current_player = 'O' if current_player == 'X' else 'X'
-            # Botão reiniciar
-            btn_rect = pygame.Rect(WIDTH//2-80, HEIGHT-50, 160, 36)
+                            current_player = 'O'
+            # Botões
+            btn_rect, back_rect = draw_board()
             if btn_rect.collidepoint(event.pos):
                 reset_board()
+            if back_rect.collidepoint(event.pos):
+                pygame.quit()
+                sys.exit()
+    # Bot joga como 'O' usando minimax
+    if not game_over and current_player == 'O':
+        pygame.time.wait(400)
+        move = best_move(board)
+        if move is not None:
+            board[move] = 'O'
+            winner = check_winner()
+            if winner:
+                game_over = True
+            else:
+                current_player = 'X'
     draw_board()
     pygame.display.flip()
     clock.tick(60)
